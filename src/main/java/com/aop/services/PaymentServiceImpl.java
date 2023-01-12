0040e056e3311a34c240e3c0cd6c18a0c40ddae2
Abhishek Tiwari <abhishek.tiwari02@motherson.com>
2023-01-12 17:08:50 +0530

    First Commit

---
+++ src/main/java/com/aop/services/PaymentServiceImpl.java
@@ -0,0 +1,22 @@
+package com.aop.services;
+
+public class PaymentServiceImpl implements PaymentServices {
+
+	public void makePayment(int amount) {
+	
+		//Payment Method
+		
+		System.out.println(amount + "Amount Debited");
+		//
+		//
+		//
+		//
+		//
+		
+		
+		System.out.println(amount +"Amount Credited");
+		
+		
+	}
+
+}
